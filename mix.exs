485418d0dd191ebb13fb1b8753d7f6da9702ed01
Andrew Noskov <anoskov@ccsteam.ru>
2016-09-02 16:03:05 +0300

    use call instead cast

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule KafkaConsumer.Mixfile do
 
   def project do
     [app: :kafka_consumer,
-     version: "1.0.0",
+     version: "1.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
